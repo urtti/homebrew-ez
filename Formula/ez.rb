a83d9d0d2ab20bf6285622e6bf27bd312aa3c6e7
Tommi Urtti <tommi@urtti.com>
2025-07-23 20:00:09 +0300

    Fix the url again, third times the charm

--- Formula/ez.rb
+++ Formula/ez.rb
@@ -1,8 +1,8 @@
 class Ez < Formula
   desc "Streamlines CLI command execution through aliases"
   homepage "https://github.com/urtti/homebrew-ez"
-  url "https://github.com/urtti/homebrew-ez/blob/main/releases/download/v0.7.0/ez-v0.7.0-macos.tar.gz"
-  sha256 "fde34177fdfb93d006284168033010b847338861dcf000e4901afda6e9cf3e8e"
+  url "https://github.com/urtti/homebrew-ez/releases/download/v0.7.0/ez-v0.7.0-macos.tar.gz"
+  sha256 "4e1b7f459158e3ee2598b3d5236735e0ce04aaf673ef35d716a2fd7a094725ec"
   license "MIT"
 
   def install
