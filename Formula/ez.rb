c87a85bf9aeaadc932f895b792e01880d61fb207
Tommi Urtti <tommi@urtti.com>
2025-07-24 22:16:09 +0300

    Update ez formula to v0.7.5, add release tarball

--- Formula/ez.rb
+++ Formula/ez.rb
@@ -1,8 +1,8 @@
 class Ez < Formula
   desc "Streamlines CLI command execution through aliases"
   homepage "https://github.com/urtti/homebrew-ez"
-  url "https://github.com/urtti/homebrew-ez/releases/download/v0.7.0/ez-v0.7.0-macos.tar.gz"
-  sha256 "4e1b7f459158e3ee2598b3d5236735e0ce04aaf673ef35d716a2fd7a094725ec"
+  url "https://github.com/urtti/homebrew-ez/releases/download/v0.7.5/ez-v0.7.5-macos.tar.gz"
+  sha256 "fdcb67d19d31b71dcd38c6455db70dd65557a268886fba0de60cb2ca69f06faf"
   license "MIT"
 
   def install
